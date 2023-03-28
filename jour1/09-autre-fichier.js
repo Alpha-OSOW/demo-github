26bd1cfbf0a06e0a37fcb90660fae7e7c911cdae
Alpha S <alphaoumar210@yahoo.fr>
2023-03-28 12:10:44 +0200

    hier

---
+++ jour1/09-autre-fichier.js
@@ -0,0 +1,19 @@
+const a = {
+    largeur : 20 ,
+    hauteur : 30 ,
+    unite : "cm"
+}; 
+
+function calcul(){
+    return `${a.largeur} x ${a.hauteur} = ${a.largeur * a.hauteur}`
+}
+
+// git branch -M menu-principal
+// git checkout master 
+// git branch -M main // renommer la branch master => main 
+// git checkout menu-principal
+// git branch -M menu
+
+// git log --oneline 
+
+// find de journée
